d937eb81d8cae67993f4ec96826394b50949bca2
yehuozhili <673632758@qq.com>
2020-06-12 02:10:05 +0800

    feat(progress): add circle progress bar

---
+++ src/components/Popconfirm/popconfirm.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+function Popconfirm(){
+    return (
+        <div></div>
+    )
+}
+
+
+
+export default Popconfirm;
+
--- src/components/Progress/progress.example.tsx
+++ src/components/Progress/progress.example.tsx
@@ -3,7 +3,6 @@ import Button from "../Button";
 import Progress from "./progress";
 
 
-
 export function ProgressExample(){
     const [count,setCount]=useState(30)
     return (
@@ -13,6 +12,20 @@ export function ProgressExample(){
             <Button onClick={()=>setCount(count+10)}>+10</Button>
             <Button onClick={()=>setCount(count-10)}>-10</Button>
             <Progress count={count}></Progress>
+        </div>
+    )
+}
+
+
+export function Progresscircle(){
+    const [count,setCount]=useState(1)
+    return (
+        <div>
+            <Button onClick={()=>setCount(count+1)}>+1</Button>
+            <Button onClick={()=>setCount(count-1)}>-1</Button>
+            <Button onClick={()=>setCount(count+10)}>+10</Button>
+            <Button onClick={()=>setCount(count-10)}>-10</Button>
+            <Progress count={count} cicrle></Progress>
         </div>
     )
 }
