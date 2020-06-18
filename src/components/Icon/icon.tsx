be254eb199ea2519ad1a3b2bc66c9e752ec23c16
yehuozhili <673632758@qq.com>
2020-06-18 16:16:23 +0800

    feat(icon): minimize icon size ; update doc;

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -2,8 +2,65 @@ import React, { FC } from "react";
 import classNames from "classnames";
 import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fas);
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons/faInfoCircle";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons/faSpinner";
+import { faAd } from "@fortawesome/free-solid-svg-icons/faAd";
+import { faAddressBook } from "@fortawesome/free-solid-svg-icons/faAddressBook";
+import { faAnchor } from "@fortawesome/free-solid-svg-icons/faAnchor";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons/faCoffee";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons/faArrowLeft";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons/faArrowDown";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons/faArrowUp";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons/faQuestionCircle";
+import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
+import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
+import { faCog } from "@fortawesome/free-solid-svg-icons/faCog";
+import { faCircle } from "@fortawesome/free-solid-svg-icons/faCircle";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons/faAngleDown";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons/faAngleLeft";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons/faAngleRight";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons/faAngleUp";
+import { faBell } from "@fortawesome/free-solid-svg-icons/faBell";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons/faTimesCircle";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons/faExclamationCircle";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons/faBookmark";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons/faCheckCircle";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons/faMapMarkerAlt";
+import { faAtom } from "@fortawesome/free-solid-svg-icons/faAtom";
+import { faFilter } from "@fortawesome/free-solid-svg-icons/faFilter";
+
+library.add(
+	faCoffee,
+	faInfoCircle,
+	faSpinner,
+	faAd,
+	faAddressBook,
+	faAnchor,
+	faArrowLeft,
+	faArrowDown,
+	faArrowRight,
+	faArrowUp,
+	faQuestionCircle,
+	faTimes,
+	faUser,
+	faSearch,
+	faCog,
+	faCircle,
+	faAngleDown,
+	faAngleLeft,
+	faAngleRight,
+	faAngleUp,
+	faBell,
+	faTimesCircle,
+	faExclamationCircle,
+	faBookmark,
+	faCheckCircle,
+	faMapMarkerAlt,
+	faAtom,
+	faFilter
+);
 
 export type ThemeProps =
 	| "primary"
